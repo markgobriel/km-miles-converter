865686963907e8e4c65948b9dc5fef9d455ed4c9
Mark Gobriel <markgobriel@gmail.com>
2022-11-09 23:03:00 -0500

    repeating process

--- km to miles.py
+++ km to miles.py
@@ -1,9 +1,23 @@
-# this is where the user inputs the km value
-km = float(input("enter a distance in km: "))
+def main():
 
-# converting km --> miles
-conv_fac = 0.621371
+    # this is where the user inputs the km value
+    km = float(input("enter a distance in km: "))
 
-# in miles
-miles = km * conv_fac
-print('%0.2f kilometers is equivalent to %0.2f miles' %(km,miles))
+    # converting km --> miles
+    conv_fac = 0.621371
+
+    # in miles
+    miles = km * conv_fac
+    print('%0.2f kilometers is equivalent to %0.2f miles' %(km,miles))
+
+    # repeat the process
+    repeat=input("do you want to convert another distance? ").lower()
+
+    if repeat == "yes":
+        main()
+
+    else:
+        print("bye!")
+        exit()
+
+main()
